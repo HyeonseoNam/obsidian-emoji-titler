793847ab4a1f5378774956e71ada25b4646e0d3e
Hyeonseo Nam <auspice7g@gmail.com>
2023-02-27 02:34:29 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,137 +1,145 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, Notice, TAbstractFile, PluginSettingTab, Setting } from "obsidian";
+import { basename, join } from 'path';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface EmojiTitlerSettings {
+  emoji1: string;
+  emoji2: string;
+  emoji3: string;
+  emoji4: string;
+  emoji5: string;
+  emoji6: string;
+  emoji7: string;
+  emoji8: string;
+  emoji9: string;
+  emoji0: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: EmojiTitlerSettings = {
+  emoji1: "🌱", 
+  emoji2: "🌿",
+  emoji3: "🌳",
+  emoji4: "📝",
+  emoji5: "📒",
+  emoji6: "📙",
+  emoji7: "📚", 
+  emoji8: "📌",
+  emoji9: "👀",
+  emoji0: "✅",
+};
+
+export default class EmojiTitlerPlugin extends Plugin {
+  settings: EmojiTitlerSettings;
+  emojiRegex = /^\p{Emoji}/u;
+
+  async onload() {
+    await this.loadSettings();
+
+    for (let i = 1; i <= 10; i++) {
+      i = i % 10;
+      this.addCommand({
+        id: `insrt-emoji${i}-title`,
+        name: `insert emoji${i} in the title`,
+        callback: async () => {
+          await this.editEmojiTitle(this.settings[`emoji${i}`] || this.emojis[i - 1]);
+        },
+        hotkeys: [
+          {
+            modifiers: ['Ctrl', 'Shift'],
+            key: `${i}`,
+          },
+        ],
+      });
+    }
+
+    this.addCommand({
+      id: 'delete-emoji-title',
+      name: 'delete emoji from the title',
+      callback: async () => {
+        await this.editEmojiTitle('');
+      },
+      hotkeys: [
+        {
+          modifiers: ['Ctrl', 'Shift'],
+          key: '-',
+        },
+      ],
+    });
+
+    this.addSettingTab(new EmojiTitlerSettingTab(this.app, this));
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async editEmojiTitle(emoji: string) {
+    const activeLeaf = this.app.workspace.activeLeaf;
+    if (!activeLeaf) {
+      return;
+    }
+
+    const file = activeLeaf.view.file;
+    if (!(file instanceof TAbstractFile)) {
+      return;
+    }
+
+    const oldPath = file.path;
+    const ext = file.extension;
+    let basenameWithoutExt = basename(oldPath, `.${ext}`);
+    
+    const match = basenameWithoutExt.match(this.emojiRegex);
+    if (match) {
+      basenameWithoutExt = basenameWithoutExt.replace(match[0], '');  
+    }
+    const newPath = join(file.parent.path, `${emoji}${basenameWithoutExt}.${ext}`);
+    await this.app.fileManager.renameFile(file, newPath);
+
+    // if (emoji == '') {
+    //   new Notice("Emoji deleted")
+    // } else {
+    //   new Notice(emoji + " titled")
+    // }
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+
+  async onunload() {
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+class EmojiTitlerSettingTab extends PluginSettingTab {
+	plugin: EmojiTitlerPlugin;
+  
+	constructor(app: App, plugin: EmojiTitlerPlugin) {
+	  super(app, plugin);
+	  this.plugin = plugin;
 	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
+  
 	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
+	  const { containerEl } = this;
+  
+	  containerEl.empty();
+  
+	  containerEl.createEl("h2", { text: "Emoji Titler Settings" });
+  
+	  for (let i = 1; i <= 10; i++) {
+    i = i % 10;
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		  .setName(`Emoji ${i}`)
+		  .setDesc(`set the emoji to be assigned to number ${i}`)
+		  .addText((text) =>
+			text
+			  .setPlaceholder(`Emoji ${i}`)
+			  .setValue(this.plugin.settings[`emoji${i}`])
+			  .onChange(async (value) => {
+				this.plugin.settings[`emoji${i}`] = value;
+				await this.plugin.saveSettings();
+			  })
+		  );
+	  }
 	}
-}
+  }
+
+
