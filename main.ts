f003fbfe16323198fc20655c21e142ccf6e4082e
Hyeonseo Nam <auspice7g@gmail.com>
2023-02-27 15:43:38 +0900

    Update main.ts: Add shortcut settings

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { App, Plugin, Notice, TAbstractFile, PluginSettingTab, Setting, TFile } from "obsidian";
+import { App, Plugin, PluginSettingTab, Setting, TFile } from "obsidian";
 import { basename, join } from 'path';
 
-type EmojiTitlerSettingsKey = "emoji1" | "emoji2" | "emoji3" | "emoji4" | "emoji5" | "emoji6" | "emoji7" | "emoji8" | "emoji9";
+type EmojiTitlerSettingsKey = "emoji1" | "emoji2" | "emoji3" | "emoji4" | "emoji5" | "emoji6" | "emoji7" | "emoji8" | "emoji9" | "emoji10";
 interface EmojiTitlerSettings {
   emoji1: string;
   emoji2: string;
@@ -12,6 +12,7 @@ interface EmojiTitlerSettings {
   emoji7: string;
   emoji8: string;
   emoji9: string;
+  emoji10: string;
 }
 
 const DEFAULT_SETTINGS: EmojiTitlerSettings = {
@@ -22,21 +23,22 @@ const DEFAULT_SETTINGS: EmojiTitlerSettings = {
   emoji5: "📒",
   emoji6: "📙",
   emoji7: "📚", 
-  emoji8: "👀",
-  emoji9: "✅",
+  emoji8: "💎",
+  emoji9: "👀",
+  emoji10: "✅",
 };
 
 export default class EmojiTitlerPlugin extends Plugin {
   settings: EmojiTitlerSettings;
   emojiRegex = /^\p{Emoji}/u;
-
+  
   async onload() {
     await this.loadSettings();
-
-    for (let i = 1; i < 10; i++) {
+    
+    for (let i = 1; i <= 10; i++) {
       this.addCommand({
         id: `insrt-emoji${i}-title`,
-        name: `insert emoji${i} in the title`,
+        name: `Insert emoji ${i} in title`,
         callback: async () => {
           await this.editEmojiTitle(this.settings[`emoji${i}` as EmojiTitlerSettingsKey]);
         },
@@ -48,7 +50,7 @@ export default class EmojiTitlerPlugin extends Plugin {
         ],
       });
     }
-
+    
     this.addCommand({
       id: 'delete-emoji-title',
       name: 'delete emoji from the title',
@@ -62,20 +64,20 @@ export default class EmojiTitlerPlugin extends Plugin {
         },
       ],
     });
-
+    
     this.addSettingTab(new EmojiTitlerSettingTab(this.app, this));
   }
-
+  
   async loadSettings() {
     this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
   }
-
+  
   async editEmojiTitle(emoji: string) {
     const file = this.app.workspace.getActiveFile();
     if (!(file instanceof TFile)) {
       return;
     }
-
+    
     const oldPath = file?.path!;
     const ext = file?.extension!;
     let basenameWithoutExt = basename(oldPath, `.${ext}`);
@@ -87,43 +89,56 @@ export default class EmojiTitlerPlugin extends Plugin {
     const newPath = join(file.parent.path, `${emoji}${basenameWithoutExt}.${ext}`);
     await this.app.fileManager.renameFile(file, newPath);
   }
-
+  
   async saveSettings() {
     await this.saveData(this.settings);
   }
-
+  
   async onunload() {
   }
 }
 
 class EmojiTitlerSettingTab extends PluginSettingTab {
-	plugin: EmojiTitlerPlugin;
+  plugin: EmojiTitlerPlugin;
   
-	constructor(app: App, plugin: EmojiTitlerPlugin) {
-	  super(app, plugin);
-	  this.plugin = plugin;
-	}
-  
-	display(): void {
-	  const { containerEl } = this;
-  
-	  containerEl.empty();
-  
-	  containerEl.createEl("h2", { text: "Emoji Titler Settings" });
-  
-	  for (let i = 1; i < 10; i++) {
-		new Setting(containerEl)
-		  .setName(`Emoji ${i}`)
-		  .setDesc(`set the emoji to be assigned to number ${i}`)
-		  .addText((text) =>
-			text
-			  .setPlaceholder(`Emoji ${i}`)
-			  .setValue(this.plugin.settings[`emoji${i}` as EmojiTitlerSettingsKey])
-			  .onChange(async (value) => {
-				this.plugin.settings[`emoji${i}` as EmojiTitlerSettingsKey] = value;
-				await this.plugin.saveSettings();
-			  })
-		  );
-	  }
-	}
+  constructor(app: App, plugin: EmojiTitlerPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
   }
+  
+  display(): void {
+    const { containerEl } = this;
+    
+    containerEl.empty();
+    containerEl.createEl("h2", { text: "Emoji Titler Settings" });
+    // new Setting(containerEl);
+    new Setting(this.containerEl)
+    .setDesc("Note! This plugin does not have default shortcuts set to prevent shortcut conflicts. Assign shortcuts for each emoji directly. A good option is to use Ctrl(Cmd)+Shift in combination with a number to assign each emoji, and the - key for deletion.")
+    .addButton((cb) => {
+      cb.setButtonText("Specify shortcuts")
+      .setCta()
+      .onClick(() => {
+        app.setting.openTabById("hotkeys");
+        const tab = app.setting.activeTab;
+        tab.searchInputEl.value = `Emoji Titler:`;
+        tab.updateHotkeyVisibility();
+      });
+    });
+    
+    containerEl.createEl("h2", { text: "Specify Emojis" });
+    for (let i = 1; i <= 10; i++) {
+      new Setting(containerEl)
+      .setName(`emoji ${i}`)
+      // .setDesc(`Specify emoji ${i} with shortcut`)
+      .addText((text) =>
+      text
+      .setPlaceholder(`emoji${i}`)
+      .setValue(this.plugin.settings[`emoji${i}` as EmojiTitlerSettingsKey])
+      .onChange(async (value) => {
+        this.plugin.settings[`emoji${i}` as EmojiTitlerSettingsKey] = value;
+        await this.plugin.saveSettings();
+      })
+      );
+    }
+  }
+}
