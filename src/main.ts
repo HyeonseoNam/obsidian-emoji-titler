c70377fdcf3c9e8b4d6d32880ebc5ec1862725ba
Hyeonseo Nam <auspice7g@gmail.com>
2023-03-12 22:43:40 +0900

    Add src directory

---
+++ src/main.ts
@@ -0,0 +1,89 @@
+import { Plugin, TFile } from "obsidian";
+import { EmojiTitlerSettingTab } from "src/settings";
+
+interface EmojiCmd {
+  id: number;
+  name: string; // TODO: will be added to frontmatter
+  emoji: string;
+}
+
+interface EmojiTitlerSettings {
+  emojis: EmojiCmd[];
+  tag_on: boolean;
+}
+
+const DEFAULT_SETTINGS: EmojiTitlerSettings = {
+  emojis: [
+    {id: 0, name: "seedling", emoji: "🌱"}, 
+    {id: 1, name: "leaves", emoji: "🌿"}, 
+    {id: 2, name: "tree", emoji: "🌳"}, 
+    {id: 3, name: "done", emoji: "✅"}, 
+    {id: 4, name: "", emoji: ""},
+  ],
+  tag_on: true
+};
+
+export default class EmojiTitlerPlugin extends Plugin {
+  settings: EmojiTitlerSettings;
+  
+  async onload() {
+    await this.loadSettings();
+    for (let i = 0; i < this.settings.emojis.length; i++) {
+      this.addCommand(this.getInsertCmd(i));
+    }
+    this.addSettingTab(new EmojiTitlerSettingTab(this.app, this));
+  }
+  
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+  
+  getInsertCmd(index: number) {
+    return {
+      id: `insert-emoji-${index}`,
+      name: `Insert emoji ${index} in title`,
+      callback: async () => {
+        await this.editEmojiTitle(this.settings.emojis[index]);
+      },
+    }
+  }
+
+  async editEmojiTitle(input_emoji: EmojiCmd) {
+    const file = this.app.workspace.getActiveFile();
+    if (!(file instanceof TFile)) {
+      return;
+    }
+
+    // check whether input emoji is in the title
+    let newName = file.basename
+    const newNameArr = [...newName];
+    
+    // if the same emoji exists in the title, just remove it.
+    if (newNameArr[0] == input_emoji.emoji) {
+      newNameArr.shift();
+      newName = newNameArr.join('');
+    } else {
+      // delete if current 
+      for (let i = 0; i < this.settings.emojis.length; i++) {
+        if (newNameArr[0] == this.settings.emojis[i].emoji) {
+          newNameArr.shift();
+          newName = newNameArr.join('');
+          break;
+        }
+      }
+      newName = `${input_emoji.emoji}${newName}`
+    }
+    // @ts-ignore
+    const newPath = file.getNewPathAfterRename(newName)
+    await this.app.fileManager.renameFile(file, newPath);
+
+    // TODO: check whether input tag is in the metadata
+  }
+  
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+  
+  async onunload() {
+  }
+}
---
+++ src/settings.ts
@@ -0,0 +1,84 @@
+import { App, PluginSettingTab, Setting } from "obsidian";
+import type EmojiTitlerPlugin from "./main";
+
+export class EmojiTitlerSettingTab extends PluginSettingTab {
+    plugin: EmojiTitlerPlugin;
+    EmojiSettings: Setting[] = [];
+    constructor(app: App, plugin: EmojiTitlerPlugin) {
+      super(app, plugin);
+      this.plugin = plugin;
+    }
+    
+    display(): void {
+      const { containerEl } = this;
+      
+      // Title & Shortcut button
+      containerEl.empty();
+      containerEl.createEl("h2", { text: "Emoji Titler Settings" });
+      new Setting(this.containerEl)
+      .setDesc("Note! This plugin does not have default shortcuts set to prevent shortcut conflicts. Assign shortcuts for each emoji directly. A good option is to use Ctrl(Cmd)+Shift in combination with a number to assign each emoji, and the - key for deletion.")
+      .addButton((cb) => {
+        cb.setButtonText("Specify shortcuts")
+        .setCta()
+        .onClick(() => {
+          // @ts-ignore
+          app.setting.openTabById("hotkeys");
+          // @ts-ignore
+          const tab = app.setting.activeTab;
+          tab.searchInputEl.value = `Emoji Titler:`;
+          tab.updateHotkeyVisibility();
+        });
+      });
+      
+      containerEl.createEl("h2", { text: "Specify Emojis" });
+  
+      // add button
+      new Setting(this.containerEl) 
+      .setDesc("Add new emoji setting / Delete the last one")
+      .addButton((cb) => {
+        cb.setIcon("plus")
+        .setCta()
+        .onClick(() => {
+          const newIndex = this.plugin.settings.emojis.length;
+          this.plugin.settings.emojis.push({id: newIndex, name: "", emoji: ""});
+          this.addEmojiSetting(newIndex);
+          this.plugin.addCommand(this.plugin.getInsertCmd(newIndex));
+        });
+      })
+      // delete button
+      .addButton((cb) => {
+        cb.setIcon("minus")
+        .onClick(() => {
+          const removedEmoji = this.plugin.settings.emojis.pop()!;
+          if (removedEmoji) {
+            const removedEmojiSet = this.EmojiSettings.pop()!;
+            this.containerEl.removeChild(removedEmojiSet.settingEl);
+            this.plugin.app.commands.removeCommand(
+                `${this.plugin.manifest.id}:${this.plugin.getInsertCmd(removedEmoji.id)['id']}`
+          )}
+        });
+      });
+      
+      // add emoji setting components
+      for (let i = 0; i < this.plugin.settings.emojis.length; i++) {
+        this.addEmojiSetting(i)
+      }
+    }
+    
+    addEmojiSetting(newIndex: number) {
+      const setting = new Setting(this.containerEl)
+        .setName(`emoji ${newIndex}`)
+        .addText((text) =>
+          text
+          .setPlaceholder(`emoji ${newIndex}`)
+          .setValue(this.plugin.settings.emojis[newIndex].emoji)
+          .onChange(async (value) => {
+            this.plugin.settings.emojis[newIndex].emoji = value;
+            await this.plugin.saveSettings();
+          })
+        );
+      // register on setting list
+      this.EmojiSettings.push(setting);
+    }
+  }
+  
